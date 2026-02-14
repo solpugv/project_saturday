c097faa6e37e7aed4107d4607527209f9bb6368e
Anna <jpegann2464257@gmail.com>
2026-02-14 12:06:11 +0300

    lasttcom

--- script.js
+++ script.js
@@ -5,15 +5,15 @@ const prevBtn = document.querySelector(".prev");
 const dots = document.querySelectorAll(".dot");
 const carousel = document.querySelector(".carousel");
 
-let index = 0;
-let intervalId;
+let index = 0; //номер текущего слайда
+let intervalId; //id прокрутки
 
 function updateCarousel() {
     track.style.transform = `translateX(-${index * 100}%)`;
 
     dots.forEach(dot => dot.classList.remove("active"));
     dots[index].classList.add("active");
-}
+}//обновление положения слайдов
 
 function nextSlide() {
     index = (index + 1) % slides.length;
@@ -46,20 +46,16 @@ function stopAutoSlide() {
 carousel.addEventListener("mouseenter", stopAutoSlide);
 carousel.addEventListener("mouseleave", startAutoSlide);
 
-carousel.addEventListener("mouseenter", stopAutoSlide);
-carousel.addEventListener("mouseleave", startAutoSlide);
 
 window.addEventListener("load", () => {
-    startAutoSlide();
+    startAutoSlide(); //запуск при загрузке страницы
 });
+
+
 document.querySelectorAll(".open-page").forEach(link => {
+    //находим все элементы с классом open page
     link.addEventListener("click", function (event) {
-        event.preventDefault(); // отменяем обычный переход
-        window.open(this.href, "_blank"); // открываем в новой вкладке
-    });
-});
-document.querySelectorAll(".open-page").forEach(link => {
-    link.addEventListener("click", function (event) {
+        //если кликнули по ссылке то
         event.preventDefault(); // отменяем обычный переход
         window.open(this.href, "_blank"); // открываем в новой вкладке
     });
