2acdc5d40bc80170379e2dd8c6aa4ae42b7e178a
Anna <jpegann2464257@gmail.com>
2026-02-14 04:11:03 +0300

    carouselandblank

---
+++ script.js
@@ -0,0 +1,66 @@
+const track = document.querySelector(".carousel-track");
+const slides = document.querySelectorAll(".carousel-track img");
+const nextBtn = document.querySelector(".next");
+const prevBtn = document.querySelector(".prev");
+const dots = document.querySelectorAll(".dot");
+const carousel = document.querySelector(".carousel");
+
+let index = 0;
+let intervalId;
+
+function updateCarousel() {
+    track.style.transform = `translateX(-${index * 100}%)`;
+
+    dots.forEach(dot => dot.classList.remove("active"));
+    dots[index].classList.add("active");
+}
+
+function nextSlide() {
+    index = (index + 1) % slides.length;
+    updateCarousel();
+}
+
+function prevSlide() {
+    index = (index - 1 + slides.length) % slides.length;
+    updateCarousel();
+}
+
+nextBtn.addEventListener("click", nextSlide);
+prevBtn.addEventListener("click", prevSlide);
+
+dots.forEach((dot, i) => {
+    dot.addEventListener("click", () => {
+        index = i;
+        updateCarousel();
+    });
+});
+
+function startAutoSlide() {
+    intervalId = setInterval(nextSlide, 4000);
+}
+
+function stopAutoSlide() {
+    clearInterval(intervalId);
+}
+
+carousel.addEventListener("mouseenter", stopAutoSlide);
+carousel.addEventListener("mouseleave", startAutoSlide);
+
+carousel.addEventListener("mouseenter", stopAutoSlide);
+carousel.addEventListener("mouseleave", startAutoSlide);
+
+window.addEventListener("load", () => {
+    startAutoSlide();
+});
+document.querySelectorAll(".open-page").forEach(link => {
+    link.addEventListener("click", function (event) {
+        event.preventDefault(); // отменяем обычный переход
+        window.open(this.href, "_blank"); // открываем в новой вкладке
+    });
+});
+document.querySelectorAll(".open-page").forEach(link => {
+    link.addEventListener("click", function (event) {
+        event.preventDefault(); // отменяем обычный переход
+        window.open(this.href, "_blank"); // открываем в новой вкладке
+    });
+});
